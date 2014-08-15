eec278719f168ed40246fde413509da9e9af73fa
oreynoldson <oli.reynoldson@gmail.com>
2014-08-15 12:46:21 -0700

    Keydown and key up
    Added functionality to the keydown and keyup for the x key.

--- js/app.js
+++ js/app.js
@@ -25,12 +25,31 @@ $(document).ready(function(){
 		console.log("Mouse up working");
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
-		//Ryu goes back to ready position
+	})
+	$(document).keydown(function(event) {
+		if(event.which == 88){
+			$(".ryu-ready").hide();
+			$(".ryu-still").hide();
+			$(".ryu-throwing").hide();
+			$(".ryu-cool").show();
+			}
+		})
+	$(document).keyup(function(event){
+		if(event.which == 88){
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		}
 	})
 });
+
 
 function playhadouken () {
 	$("#hadouken-sound")[0].volume = 0.5;
 	$("#hadouken-sound")[0].load();
 	$("#hadouken-sound")[0].play();
 }
+
+// Variable for when mouse is still hovering over Ryu
+
+
+var mouseout = $(".ryu").mouseleave();
